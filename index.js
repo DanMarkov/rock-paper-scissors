91f3ca055f95aaa50deccaba5a9a4d788710f830
Kenny <89842110+DanMarkov@users.noreply.github.com>
2023-12-30 15:27:37 +0800

    feat: improve logic in playRound function
    Inside of playRound function I added conditions with interactive
    interplay with computer and user. Also I added game function
    that includes loop iterations of 5 rounds in order to
    continue playing the game.

--- index.js
+++ index.js
@@ -11,17 +11,39 @@ function getComputerChoice() {
 
 
 function playRound(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection.toLowerCase(); 
     if (playerSelection === computerSelection) {
         return "It's a tie!";
     } else if (playerSelection !== computerSelection) {
-        if (computerSelection === "paper") {
-            return "Oh, damn, you lose by a paper...";
-        } else {
+        if (computerSelection === "paper" && playerSelection === "rock") {
+            return "Oh, damn, you lose by a paper..."; 
+        } else if (computerSelection === "paper" && playerSelection === "scissors") {
+            return "Hooray! You win, scissors beat paper =)";
+        } else if (computerSelection === "scissors" && playerSelection === "rock") {
             return "Hooray! You win, a rock beats scissors =)";
+        } else if (computerSelection === "scissors" && playerSelection === "paper") {
+            return "Oh, damn, you lose by scissors...";
+        } else if (computerSelection === "rock" && playerSelection === "paper") {
+            return "Hooray! You win, paper beats rock =)";
+        } else if (computerSelection === "rock" && playerSelection === "scissors") {
+            return "Oh, damn, you lose by a rock...";
+        } else {
+            return "please choose rock, paper or scissors only!";
         }
     }
 }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    for (let round = 0; round < 5; round++) {
+       const playerSelection = prompt("Choose between rock, paper and scissors");
+       const computerSelection = getComputerChoice();
+       let gameplay = playRound(playerSelection, computerSelection);
+       console.log(gameplay);
+       if (gameplay === "It's a tie!" || gameplay === "please choose rock, paper or scissors only!") {
+        round--;
+       }
+    }
+}
+
+console.log(game());
