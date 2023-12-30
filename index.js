f4389a73fa34fa5e7124f417bc6b27c074837ef7
Kenny <89842110+DanMarkov@users.noreply.github.com>
2023-12-30 17:33:18 +0800

    feat: improve the logic to complete a game
    With this commit the console counts user and computer points
    and shows who has won.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
-    playerSelection = playerSelection.toLowerCase(); 
+    playerSelection = playerSelection.toLowerCase().trim(); 
     if (playerSelection === computerSelection) {
         return "It's a tie!";
     } else if (playerSelection !== computerSelection) {
@@ -35,15 +35,31 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
+    let computerCount = 0;
+    let userCount = 0;
     for (let round = 0; round < 5; round++) {
-       const playerSelection = prompt("Choose between rock, paper and scissors");
-       const computerSelection = getComputerChoice();
-       let gameplay = playRound(playerSelection, computerSelection);
-       console.log(gameplay);
-       if (gameplay === "It's a tie!" || gameplay === "please choose rock, paper or scissors only!") {
-        round--;
-       }
+        const playerSelection = prompt("Choose between rock, paper and scissors");
+        const computerSelection = getComputerChoice();
+        let gameplay = playRound(playerSelection, computerSelection);
+        console.log(gameplay);
+
+        if (gameplay === "It's a tie!" || gameplay === "please choose rock, paper or scissors only!") {
+            round--;  
+        } else if (gameplay === "Oh, damn, you lose by a paper..." || gameplay === "Oh, damn, you lose by scissors..." || gameplay === "Oh, damn, you lose by a rock...") {
+            computerCount++;
+        } else if (gameplay === "Hooray! You win, scissors beat paper =)" || gameplay === "Hooray! You win, a rock beats scissors =)" || gameplay === "Hooray! You win, paper beats rock =)") {
+            userCount++; 
+        } 
+
+    }
+
+    console.info("Computer: " + computerCount);
+    console.info("User: " + userCount);
+    if (computerCount > userCount) {
+        return console.log("Computer has won!");
+    } else if (computerCount < userCount) {
+        return console.log("User has won!");
     }
 }
 
-console.log(game());
+game();
