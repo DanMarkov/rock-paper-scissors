0c0c5af657009600bf703ad6fb139fc3a5f67f36
Kenny <89842110+DanMarkov@users.noreply.github.com>
2024-01-31 17:31:19 +0800

    feat: add interactive buttons to play RPS
    With this commit I've created buttons via DOM JS to play RPS
    inside the console.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+
+let buttons = [];
+
+for (let i = 0; i < 3; i++) {
+    buttons[i] = document.createElement("button");
+}
+
+buttons[0].textContent = "rock";
+buttons[1].textContent = "paper";
+buttons[2].textContent = "scissors";
+
+document.body.append(buttons[0], buttons[1], buttons[2]);
+
+
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3) + 1;
     if (random === 1) {
@@ -9,53 +23,62 @@ function getComputerChoice() {
     }
 }
 
+for (button of buttons) {
+    button.addEventListener("click", playRound);
+}
 
-function playRound(playerSelection, computerSelection) {
 
-    playerSelection = playerSelection.toLowerCase().trim(); 
+function playRound() {
+
+       let playerSelection = this.textContent;
+    
+    
+    computerSelection = getComputerChoice();
+        
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
+        console.log("It's a tie!");
     } else if (playerSelection !== computerSelection) {
         if (computerSelection === "paper" && playerSelection === "rock") {
-            return "Oh, damn, you lose by a paper..."; 
+            console.log("Oh, damn, you lose by a paper..."); 
         } else if (computerSelection === "paper" && playerSelection === "scissors") {
-            return "Hooray! You win, scissors beat paper =)";
+            console.log("Hooray! You win, scissors beat paper =)");
         } else if (computerSelection === "scissors" && playerSelection === "rock") {
-            return "Hooray! You win, a rock beats scissors =)";
+            console.log("Hooray! You win, a rock beats scissors =)");
         } else if (computerSelection === "scissors" && playerSelection === "paper") {
-            return "Oh, damn, you lose by scissors...";
+            console.log("Oh, damn, you lose by scissors...");
         } else if (computerSelection === "rock" && playerSelection === "paper") {
-            return "Hooray! You win, paper beats rock =)";
+            console.log("Hooray! You win, paper beats rock =)");
         } else if (computerSelection === "rock" && playerSelection === "scissors") {
-            return "Oh, damn, you lose by a rock...";
+            console.log("Oh, damn, you lose by a rock...");
         } else {
-            return "please choose rock, paper or scissors only!";
+            console.log("please choose rock, paper or scissors only!");
         }
+    
     }
 }
 
 function game() {
-    let computerCount = 0;
-    let userCount = 0;
-    for (let round = 0; round < 5; round++) {
-        const playerSelection = prompt("Choose between rock, paper and scissors");
-        const computerSelection = getComputerChoice();
-        let gameplay = playRound(playerSelection, computerSelection);
-        console.log(gameplay);
+    // let computerCount = 0;
+    // let userCount = 0;
+    // for (let round = 0; round < 5; round++) {
+    //     const playerSelection = prompt("Choose between rock, paper and scissors");
+    //     const computerSelection = getComputerChoice();
+    //     let gameplay = playRound(playerSelection, computerSelection);
+    //     console.log(gameplay);
 
-        if (gameplay === "It's a tie!" || gameplay === "please choose rock, paper or scissors only!") {
-            round--;  
-        } else if (gameplay === "Oh, damn, you lose by a paper..." || 
-                   gameplay === "Oh, damn, you lose by scissors..." || 
-                   gameplay === "Oh, damn, you lose by a rock...") {
-            computerCount++;
-        } else if (gameplay === "Hooray! You win, scissors beat paper =)" || 
-                   gameplay === "Hooray! You win, a rock beats scissors =)" || 
-                   gameplay === "Hooray! You win, paper beats rock =)") {
-            userCount++; 
-        } 
+    //     if (gameplay === "It's a tie!" || gameplay === "please choose rock, paper or scissors only!") {
+    //         round--;  
+    //     } else if (gameplay === "Oh, damn, you lose by a paper..." || 
+    //                gameplay === "Oh, damn, you lose by scissors..." || 
+    //                gameplay === "Oh, damn, you lose by a rock...") {
+    //         computerCount++;
+    //     } else if (gameplay === "Hooray! You win, scissors beat paper =)" || 
+    //                gameplay === "Hooray! You win, a rock beats scissors =)" || 
+    //                gameplay === "Hooray! You win, paper beats rock =)") {
+    //         userCount++; 
+    //     } 
 
-    }
+    // }
 
     console.info("Computer: " + computerCount);
     console.info("User: " + userCount);
@@ -66,4 +89,4 @@ function game() {
     }
 }
 
-game();
+// game();
